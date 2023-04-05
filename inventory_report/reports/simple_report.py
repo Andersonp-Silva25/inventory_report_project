11ebeb34be0e4d3b546ad8cd45dffeb0a03b3a00
Anderson Pereira da Silva <andersonp.silva25@gmail.com>
2023-04-04 21:59:12 -0300

    :sparkles: Feat: Generate a simplified version of the report

--- inventory_report/reports/simple_report.py
+++ inventory_report/reports/simple_report.py
@@ -1,2 +1,33 @@
+from datetime import datetime
+from collections import Counter
+
+
 class SimpleReport:
-    pass
+
+    def __date_validator(productList):
+        manufacturing_date = min([
+            product["data_de_fabricacao"] for product in productList
+        ])
+
+        expiration_date = min([
+            product["data_de_validade"] for product in productList
+            if product["data_de_validade"] >= datetime.now()
+            .strftime('%Y-%m-%d')
+        ])
+
+        return [manufacturing_date, expiration_date]
+
+    @staticmethod
+    def generate(productList):
+
+        validated_date = SimpleReport.__date_validator(productList)
+
+        company = Counter(
+            [product["nome_da_empresa"] for product in productList]
+        ).most_common()[0][0]
+
+        return (
+            f"Data de fabricação mais antiga: {validated_date[0]}\n"
+            f"Data de validade mais próxima: {validated_date[1]}\n"
+            f"Empresa com mais produtos: {company}"
+        )
