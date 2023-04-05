a1e280049690c468c437f9e43413878c3f8f375e
Anderson Pereira da Silva <andersonp.silva25@gmail.com>
2023-04-05 15:16:03 -0300

    :sparkles: Feat: Generate the full version of the report

--- inventory_report/reports/complete_report.py
+++ inventory_report/reports/complete_report.py
@@ -1,2 +1,20 @@
-class CompleteReport:
-    pass
+from inventory_report.reports.simple_report import SimpleReport
+from collections import Counter
+
+
+class CompleteReport(SimpleReport):
+
+    @staticmethod
+    def generate(productList):
+        companies = [product["nome_da_empresa"] for product in productList]
+        quantity_company = Counter(companies).most_common()
+
+        response = (
+            f"{SimpleReport.generate(productList)}\n"
+            f"Produtos estocados por empresa:\n"
+        )
+
+        for company, quantity in quantity_company:
+            response += f"- {company}: {quantity}\n"
+
+        return response
