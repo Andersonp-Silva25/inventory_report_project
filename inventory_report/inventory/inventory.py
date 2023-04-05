e125d318abc2d96b135b8e38f483d24e41f672d7
Anderson Pereira da Silva <andersonp.silva25@gmail.com>
2023-04-05 17:30:01 -0300

    :sparkles: Feat: Generate reports from a JSON file

--- inventory_report/inventory/inventory.py
+++ inventory_report/inventory/inventory.py
@@ -1,16 +1,34 @@
+import os
 import csv
+import json
 from inventory_report.reports.simple_report import SimpleReport
 from inventory_report.reports.complete_report import CompleteReport
 
 
 class Inventory:
-    def read(path):
+    def __file_extension(path):
+        extension = os.path.splitext(path)
+        return extension[1]
+
+    def __csv_file_reader(path):
         with open(path, encoding="utf-8") as file:
-            file_reader = csv.DictReader(file, delimiter=",", quotechar='"')
-            return list(file_reader)
+            return list(
+                csv.DictReader(file, delimiter=",", quotechar='"')
+            )
+
+    def __json_file_reader(path):
+        with open(path, encoding="utf-8") as file:
+            return json.load(file)
+
+    def __file_reader(path):
+        extension = Inventory.__file_extension(path)
+        if extension == '.csv':
+            return Inventory.__csv_file_reader(path)
+        elif extension == '.json':
+            return Inventory.__json_file_reader(path)
 
     def import_data(path, type):
         if type == 'simples':
-            return SimpleReport.generate(Inventory.read(path))
+            return SimpleReport.generate(Inventory.__file_reader(path))
         elif type == 'completo':
-            return CompleteReport.generate(Inventory.read(path))
+            return CompleteReport.generate(Inventory.__file_reader(path))
